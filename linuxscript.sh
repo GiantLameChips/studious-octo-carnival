a57bc940963102bcedbbcafa095cab5668de5d38
oatzs <110140678+oatzs@users.noreply.github.com>
2023-10-05 22:34:40 -0400

    Update linuxscript.sh

--- linuxscript.sh
+++ linuxscript.sh
@@ -1,87 +1,41 @@
 
-	chattr -i /etc/passwd
-        chattr -i /etc/passwd
-	chattr -i /etc/profile
-	chattr -i /etc/bash.bashrc
-	chattr -i /etc/login.defs
-	chattr -i /etc/pam.d/common-auth
-	chattr -i /etc/pam.d/common-password
-	chattr -i /etc/group
-	chattr -i /etc/shadow
-	chattr -i /etc/ssh/sshd_config
-	chattr -i /etc/host.conf
-	chattr -i /etc/hosts.deny
-	chattr -i /etc/hosts.allow
-	chattr -i /etc/hosts
-	chattr -i /etc/resolv.conf
-	chattr -i /etc/default/grub
-	chattr -i /etc/grub.d/40_custom
-	chattr -i /etc/ers
-	chattr -i ~/.mozilla/firefox/*.default/prefs.js
-	chattr -i /etc/sysctl.conf
-	chattr -i /etc/apt/sources.list
+#!/bin/bash
 
-        chown root:root /etc/fstab
-	chmod 644 /etc/fstab
-	chown root:root /etc/group
-	chmod 644 /etc/group
-	chown root:root /etc/shadow
-	chmod 400 /etc/shadow
-	chown root:root /etc/apache2
-	chmod 755 /etc/apache2
-	chmod 0600 /etc/securetty
-	chmod 644 /etc/crontab
-	chmod 640 /etc/ftpusers
-	chmod 440 /etc/inetd.conf
-	chmod 440 /etc/xinetd.conf
-	chmod 400 /etc/inetd.d
-	chmod 644 /etc/hosts.allow
-	chmod 440 /etc/ers
-	chmod 640 /etc/shadow
-	chmod 600 /boot/grub/grub.cfg
-	chmod 600 /etc/ssh/sshd_config
-	chmod 600 /etc/gshadow-
-	chmod 600 /etc/group-
-	chmod 600 /etc/passwd-
- 
-	chown root:root /etc/ssh/sshd_config
-	chown root:root /etc/passwd-
-	chown root:root /etc/group-
-	chown root:root /etc/shadow
-	chown root:root /etc/securetty
-	chown root:root /boot/grub/grub.cfg
-	chmod og-rwx /boot/grub/grub.cfg
-	chown root:shadow /etc/shadow-
-	chmod o-rwx,g-rw /etc/shadow-
-	chown root:shadow /etc/gshadow-
-	chmod o-rwx,g-rw /etc/gshadow-
-	touch /etc/cron.allow
-	touch /etc/at.allow
-	chmod og-rwx /etc/cron.allow
-	chmod og-rwx /etc/at.allow
-	chown root:root /etc/cron.allow
-	chown root:root /etc/at.allow
-	chown root:root /etc/cron.d
-	chmod og-rwx /etc/cron.d
-	chown root:root /etc/crontab
-	chmod og-rwx /etc/crontab
-	chmod -R g-wx,o-rwx /var/log/
+apt update -y
+apt install apparmor -y
+apt install apparmor-profiles -y
+apt install apparmor-utils -y
+apt install ranger -y
+apt install fail2ban -y
+apt install tmux -y
+apt install ufw -y
+apt install install libapache2-mod-security2 -y    
 
+a2enmod userdir
+a2enmod headers
+a2enmod rewrite
+a2dismod imap
+a2dismod include
+a2dismod info
+a2dismod userdir
 
-  	apt install apparmor -y
-	apt install apparmor-profiles -y
-	apt install apparmor-utils -y
-	apt install ranger -y
-	apt install fail2ban -y
+sudo chown -R root:root /etc/apache2
 
- 	#removing nopasswdlogon group
-	echo "Removing nopasswdlogon group"
-	sed -i -e '/nopasswdlogin/d' /etc/group
+#removing nopasswdlogon group
+echo "Removing nopasswdlogon group"
+sed -i -e '/nopasswdlogin/d' /etc/group
 
-	 passwd -l root
+passwd -l root
 
-	 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-	 echo "Protocol 2" >> /etc/ssh/sshd_config
-	 echo "LoginGRaceTime 2m" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "Protocol 2" >> /etc/ssh/sshd_config
 
-         ufw deny 4444
+#metasploit default port
+ufw deny 4444
+ufw allow http
+ufw allow https
+ufw allow apache
+ufw allow ssh 
+ufw allow ftp
+  	
+
