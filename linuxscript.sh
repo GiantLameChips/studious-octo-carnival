be292ad8f1b96e949095d2d7ae2bd31b5108c9c6
oatzs <110140678+oatzs@users.noreply.github.com>
2023-10-07 17:46:19 -0400

    Update linuxscript.sh

--- linuxscript.sh
+++ linuxscript.sh
@@ -1,15 +1,26 @@
 
 #!/bin/bash
 
+passwd -l root
+
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "Protocol 2" >> /etc/ssh/sshd_config
+
+apt install ufw -y
+#metasploit default port
+ufw deny 4444
+
+ufw allow 'Apache Secure'
+ufw allow OpenSSH
+ufw allow ftp
+  	
+
 apt update -y
-apt install apparmor -y
-apt install apparmor-profiles -y
-apt install apparmor-utils -y
 apt install ranger -y
 apt install fail2ban -y
 apt install tmux -y
-apt install ufw -y
 apt install curl -y
+apt install whowatch -y
 apt install install libapache2-mod-security2 -y    
 
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
@@ -29,16 +40,5 @@ sudo chown -R root:root /etc/apache2
 echo "Removing nopasswdlogon group"
 sed -i -e '/nopasswdlogin/d' /etc/group
 
-passwd -l root
 
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "Protocol 2" >> /etc/ssh/sshd_config
-
-#metasploit default port
-ufw deny 4444
-
-ufw allow 'Apache Secure'
-ufw allow OpenSSH
-ufw allow ftp
-  	
 
