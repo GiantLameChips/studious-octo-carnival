ec080952fec4b7ee9578440e9503387f4fea56e0
oatzs <110140678+oatzs@users.noreply.github.com>
2023-10-07 20:02:23 -0400

    Update linuxscript.sh

--- linuxscript.sh
+++ linuxscript.sh
@@ -13,7 +13,7 @@ ufw deny 4444
 ufw allow 'Apache Secure'
 ufw allow OpenSSH
 ufw allow ftp
-  	
+ufw enable
 
 apt update -y
 apt install ranger -y
@@ -39,6 +39,31 @@ sudo chown -R root:root /etc/apache2
 #removing nopasswdlogon group
 echo "Removing nopasswdlogon group"
 sed -i -e '/nopasswdlogin/d' /etc/group
+
+echo "hkeating" >> /etc/vsftpd.chroot_list
+# Jail users to home directory (user will need a home dir to exist)
+echo "chroot_local_user=YES" >> /etc/vsftpd.conf
+echo "chroot_list_enable=YES" >> /etc/vsftpd.conf
+echo "chroot_list_file=/etc/vsftpd.chroot_list" >> /etc/vsftpd.conf
+echo "allow_writeable_chroot=NO" >> /etc/vsftpd.confe
+
+# Allow or deny users
+echo "hkeating" >> /etc/vsftpd.userlist
+echo "userlist_enable=YES" >> /etc/vsftpd.userlist
+echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
+echo "userlist_deny=NO" >> /etc/vsftpd.conf
+
+# General config
+echo "anonymous_enable=NO" >> /etc/vsftpd.conf
+echo "local_enable=YES" >> /etc/vsftpd.conf
+echo "write_enable=YES" >> /etc/vsftpd.conf
+echo "dirmessage_enable=NO" >> /etc/vsftpd.conf
+echo "xferlog_enable=YES" >> /etc/vsftpd.conf
+echo "listen=NO" >> /etc/vsftpd.conf
+echo "userlist_enable=YES" >> /etc/vsftpd.conf
+
+echo "ascii_upload_enable=NO" >> /etc/vsftpd.conf
+echo "ascii_download_enable=NO" >> /etc/vsftpd.conf
 
 
 
