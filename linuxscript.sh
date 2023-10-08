7d71a4bf51a1231719d21dda577cc557ad714ec3
oatzs <110140678+oatzs@users.noreply.github.com>
2023-10-07 21:02:09 -0400

    Update linuxscript.sh
    fixed vsftpd.conf

--- linuxscript.sh
+++ linuxscript.sh
@@ -10,11 +10,13 @@ apt install ufw -y
 #metasploit default port
 ufw deny 4444
 
+#sets firewall rules
 ufw allow 'Apache Secure'
 ufw allow OpenSSH
 ufw allow ftp
 ufw enable
 
+#updates the repo so we can download our very useful tools
 apt update -y
 apt install ranger -y
 apt install fail2ban -y
@@ -26,37 +28,29 @@ apt install install libapache2-mod-security2 -y
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 chmod +x pspy64
 
-a2enmod userdir
-a2enmod headers
-a2enmod rewrite
-a2dismod imap
-a2dismod include
-a2dismod info
-a2dismod userdir
-
 sudo chown -R root:root /etc/apache2
 
 #removing nopasswdlogon group
 echo "Removing nopasswdlogon group"
 sed -i -e '/nopasswdlogin/d' /etc/group
 
-chattr +i /files/Seabiscuit.jpg
+chmod 644 /etc/passwd
 
-# Allow or deny users
+chattr +i /files/Seabiscuit.jpg
+chattr +i /etc/vsftpd.userlist
+
+#allow only the scoring user
 echo "hkeating" >> /etc/vsftpd.userlist
 echo "userlist_enable=YES" >> /etc/vsftpd.userlist
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 echo "userlist_deny=NO" >> /etc/vsftpd.conf
+echo "chroot_local_user=NO" >> /etc/vsftpd.conf
 
-# General config
+#general
 echo "anonymous_enable=NO" >> /etc/vsftpd.conf
 echo "local_enable=YES" >> /etc/vsftpd.conf
 echo "write_enable=YES" >> /etc/vsftpd.conf
-echo "dirmessage_enable=NO" >> /etc/vsftpd.conf
 echo "xferlog_enable=YES" >> /etc/vsftpd.conf
-echo "listen=NO" >> /etc/vsftpd.conf
-echo "userlist_enable=YES" >> /etc/vsftpd.conf
-
 echo "ascii_upload_enable=NO" >> /etc/vsftpd.conf
 echo "ascii_download_enable=NO" >> /etc/vsftpd.conf
 
